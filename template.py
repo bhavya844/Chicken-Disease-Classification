ecb32e05b86b6364bb11ebdd4e5f5b6fd8086e77
bhavya844 <bhavyadave700@gmail.com>
2024-05-16 19:01:57 -0300

    requirements added

--- template.py
+++ template.py
@@ -19,7 +19,8 @@ list_of_files= [".github/workflows/.gitkeep",
                 "params.yaml",
                 "requirements.txt",
                 "setup.py",
-                "research/trials.ipynb"]
+                "research/trials.ipynb",
+                "templates/index.html"]
 
 
 
